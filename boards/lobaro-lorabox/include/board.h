2b05377744c361ec8f597f4bc0a9321f95f5fc2a
kYc0o <fco.ja.ac@gmail.com>
2019-03-29 17:45:58 +0100

    boards/lobaro-lorabox: add DIOs extra pin definitions

--- boards/lobaro-lorabox/include/board.h
+++ boards/lobaro-lorabox/include/board.h
@@ -66,6 +66,10 @@ extern "C" {
 
 #define SX127X_PARAM_DIO3               GPIO_PIN(PORT_B, 7)
 
+#define SX127X_PARAM_DIO4               GPIO_PIN(PORT_B, 5)
+
+#define SX127X_PARAM_DIO5               GPIO_PIN(PORT_B, 4)
+
 #define SX127X_PARAM_PASELECT           (SX127X_PA_RFO)
 
 #define SX127X_PARAMS                   {   .spi       = SX127X_PARAM_SPI,     \
@@ -75,6 +79,8 @@ extern "C" {
                                             .dio1_pin  = SX127X_PARAM_DIO1,    \
                                             .dio2_pin  = SX127X_PARAM_DIO2,    \
                                             .dio3_pin  = SX127X_PARAM_DIO3,    \
+											.dio4_pin  = SX127X_PARAM_DIO4,    \
+											.dio5_pin  = SX127X_PARAM_DIO5,    \
                                             .paselect  = SX127X_PARAM_PASELECT \
                                         }
 /** @} */
