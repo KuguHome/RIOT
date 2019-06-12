1eff770b82ea31425fba14b6c4583247460cbb10
kYc0o <fco.ja.ac@gmail.com>
2019-06-12 15:48:00 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! tests: add wireless m-bus test

--- tests/wmbus/main.c
+++ tests/wmbus/main.c
@@ -34,6 +34,8 @@
 #include "net/netdev.h"
 #include "net/lora.h"
 
+#include "three_out_of_six.h"
+
 #include "board.h"
 
 #include "sx127x_internal.h"
@@ -41,51 +43,277 @@
 #include "sx127x_netdev.h"
 #include "sx127x_registers.h"
 
+#define ENABLE_DEBUG (1)
+#include "debug.h"
+
 #define SX127X_LORA_MSG_QUEUE   (16U)
 #define SX127X_STACKSIZE        (THREAD_STACKSIZE_DEFAULT)
 
 #define MSG_TYPE_ISR            (0x3456)
 
 static char stack[SX127X_STACKSIZE];
+//static char sx1272_continuous_stack[THREAD_STACKSIZE_DEFAULT];
 static kernel_pid_t _recv_pid;
+//static kernel_pid_t _sx127x_cont_pid;
 
 static char message[32];
 static uint8_t fsk_raw_bytes[290];
+static uint16_t fsk_raw_len;
 static sx127x_t sx127x;
 
 static uint32_t ps_detector = 0;
 static uint8_t data = 0;
+static uint8_t bit_cnt = 0;
+static uint8_t l_field_raw[3];
+static uint8_t l_field_raw_cnt = 0;
+static uint8_t raw_data_cnt = 0;
+static uint8_t wmbus_l_field[2];
 
 static const uint32_t ACCESS_CODE = 0b0101010101010000111101U;
 static const uint32_t ACCESS_CODE_BITMASK = 0x3FFFFFU;
 
+                             /* 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 */
+uint8_t fsk_reg_settings [] = { 0x0D, 0x03, 0xD4, 0x02, 0x6F, 0xD9, 0x3C, 0xCD,
+                             /* 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 */
+                                0x0F, 0x19, 0x2B, 0x20, 0x0E, 0x02, 0x0A, 0xFF,
+                             /* 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18 */
+                                0xEC, 0x02, 0x0B, 0x28, 0x0C, 0x12, 0x47, 0x32,
+                             /* 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20 */
+                                0x3E, 0x00, 0x00, 0x00, 0xFF, 0x65, 0x8A, 0x00,
+                             /* 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28 */
+                                0x00, 0x00, 0x00, 0x0F, 0x00, 0x18, 0x91, 0x54,
+                             /* 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30 */
+                                0x3D, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x10,
+                             /* 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38 */
+                                0x40, 0x1B, 0x00, 0x00, 0x8F, 0x00, 0x00, 0x00,
+                                0xF5, 0x20, 0x02, 0x00, 0x02, 0xDB, 0x26, 0x01,
+                                0xF0, 0x22, 0x13, 0x0E, 0x5B, 0xDB, 0x24, 0x00,
+                                0x01, 0x3A, 0x2E, 0x00, 0x03, 0x00, 0x00, 0x00,
+                                0x00, 0x04, 0x23, 0x00, 0x00, 0x00, 0x00, 0x09,
+                                0x05, 0x84, 0x0B, 0xD0, 0x0B, 0xD0, 0x32, 0x2B,
+                                0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F,
+                                0xE0, 0x00, 0x0C, 0x00, 0x13, 0x26, 0x01, 0x0B,
+                                0x5C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+
+static uint16_t byte_size(uint16_t packet_size)
+{
+    uint16_t tmode_var;
+
+    // T-mode
+    // Data is 3 out of 6 coded
+    tmode_var = (3 * packet_size) / 2;
+
+    if (packet_size % 2) {
+        return (tmode_var + 1);
+    }
+    else {
+        return (tmode_var);
+    }
+}
+
+static uint16_t packet_size (uint8_t l_field)
+{
+  uint16_t nr_bytes;
+  uint8_t  nr_blocks;
+
+  // The 2 first blocks contains 25 bytes when excluding CRC and the L-field
+  // The other blocks contains 16 bytes when excluding the CRC-fields
+  // Less than 26 (15 + 10)
+  if ( l_field < 26 )
+    nr_blocks = 2;
+  else
+    nr_blocks = (((l_field - 26) / 16) + 3);
+
+  // Add all extra fields, excluding the CRC fields
+  nr_bytes = l_field + 1;
+
+  // Add the CRC fields, each block is contains 2 CRC bytes
+  nr_bytes += (2 * nr_blocks);
+
+  return (nr_bytes);
+}
+
+static void _print_bytes(uint8_t *bytes, size_t size)
+{
+    for (size_t i = 0; i < size; i++) {
+        printf("%02X", bytes[i]);
+    }
+
+    puts("");
+}
+
+static int _recv(netdev_t *netdev, void *buf, size_t len, void *info)
+{
+    sx127x_t *dev = (sx127x_t*) netdev;
+    uint8_t enc_l_field[3];
+    uint8_t l_field[2];
+    uint16_t size = 0;
+    uint16_t discarded = 0;
+
+    switch (dev->settings.modem) {
+        case SX127X_MODEM_FSK:
+            /* Process the payload */
+            if (len == 0 && buf == NULL) {
+                DEBUG("Read length first...\n");
+//                if ((dev->settings.fsk.flags & SX127X_FIX_LEN_FLAG) == true) {
+                    /* Read only the length value */
+                while (!(sx127x_reg_read(dev, SX127X_REG_IRQFLAGS2) & SX127X_RF_IRQFLAGS2_FIFOEMPTY)) {
+                    sx127x_read_fifo(dev, &fsk_raw_bytes[discarded], 1);
+                    discarded++;
+                }
+                    DEBUG("Raw FSK: ");
+                    _print_bytes(fsk_raw_bytes, discarded);
+                    DEBUG("%d bytes\n", discarded);
+                    memcpy(enc_l_field, fsk_raw_bytes, 3);
+                    if (three_out_of_six_decode(enc_l_field, l_field) == THREE_OUT_OF_SIX_OK) {
+                        dev->settings.fsk.pkt_handler.size = l_field[0];
+                        fsk_raw_len = byte_size(packet_size(l_field[0]));
+                        size = fsk_raw_len;
+                        DEBUG("[sx127x] netdev->_recv: received %d bytes\n", dev->settings.fsk.pkt_handler.size);
+                        DEBUG("[sx127x] netdev->_recv: decoded L-Field %d\n", fsk_raw_len);
+                        break;
+                    }
+                    else {
+                        DEBUG("Decoding error! Cleaning FIFO\n");
+                        discarded = 0;
+                        while (!(sx127x_reg_read(dev, SX127X_REG_IRQFLAGS2) & SX127X_RF_IRQFLAGS2_FIFOEMPTY)) {
+                            sx127x_read_fifo(dev, &fsk_raw_bytes[discarded], 1);
+                            discarded++;
+                        }
+                        DEBUG("FIFO emptied: %d bytes\n", discarded);
+                        _print_bytes(fsk_raw_bytes, discarded);
+                        memset(fsk_raw_bytes, 0, sizeof(fsk_raw_bytes));
+                    }
+//                }
+//                else {
+//                    dev->settings.fsk.pkt_handler.size = sx127x_reg_read(dev, SX127X_REG_PAYLOADLENGTH);
+//                    break;
+//                }
+            }
+            else {
+                if ((dev->settings.fsk.flags & SX127X_RX_FSK_CONTINUOUS_FLAG) == false) {
+                    //DEBUG("[sx127x] netdev: Packet mode -> going IDLE\n");
+                    //sx127x_set_standby(dev);
+                }
+                else {
+                    /* Restart RX */
+                    sx127x_reg_write(dev, SX127X_REG_RXCONFIG,
+                                     sx127x_reg_read(dev, SX127X_REG_RXCONFIG) |
+                                     SX127X_RF_RXCONFIG_RESTARTRXWITHOUTPLLLOCK);
+                }
+
+                while (!(sx127x_reg_read(dev, SX127X_REG_IRQFLAGS2) & SX127X_RF_IRQFLAGS2_FIFOEMPTY)) {
+                    sx127x_read_fifo(dev, buf + dev->settings.fsk.pkt_handler.nb_bytes,
+                            dev->settings.fsk.pkt_handler.size -
+                            dev->settings.fsk.pkt_handler.nb_bytes);
+
+                    dev->settings.fsk.pkt_handler.nb_bytes += (dev->settings.fsk.pkt_handler.size -
+                            dev->settings.fsk.pkt_handler.nb_bytes);
+                }
+                sx127x_read_fifo(dev, buf, len);
+                size = len;
+            }
+
+            netdev_sx127x_packet_info_t *packet_info = info;
+            packet_info->rssi = dev->settings.fsk.pkt_handler.rssi_value;
+
+            /* Reset status */
+            dev->settings.fsk.flags &= ~SX127X_PREAMBLE_DETECTED_FLAG;
+            dev->settings.fsk.flags &= ~SX127X_SYNC_WORD_DETECTED_FLAG;
+            dev->settings.fsk.pkt_handler.nb_bytes = 0;
+            dev->settings.fsk.pkt_handler.size = 0;
+            uint8_t state = NETOPT_STATE_RX;
+            netdev->driver->set(netdev, NETOPT_STATE, &state, sizeof(state));
+
+        break;
+        case SX127X_MODEM_LORA:
+            break;
+        default:
+            break;
+    }
+
+    return size;
+}
 
 static void _dio1_data_recv(void *arg)
 {
     /* Get interrupt context */
     sx127x_t *dev = (sx127x_t *) arg;
 
-//    puts("DCLK");
-
     uint8_t bit = gpio_read(dev->params.dio2_pin);
 
-//    if (bit) {
-//        puts("Got something!");
-//    }
+    if (!data) {
+        ps_detector = (ps_detector << 1) | bit;
+    }
+    else {
+        if (l_field_raw_cnt < 3) {
+            if (bit_cnt < 8) {
+                l_field_raw[l_field_raw_cnt] = (l_field_raw[l_field_raw_cnt] << 1) | bit;
+                bit_cnt++;
+            }
+            else {
+                l_field_raw_cnt++;
+                bit_cnt = 0;
+            }
 
-    ps_detector = (ps_detector << 1) | bit;
+            if (l_field_raw_cnt == 3) {
+                if (three_out_of_six_decode(l_field_raw, wmbus_l_field) != THREE_OUT_OF_SIX_ERROR) {
+                    printf("L-Field: %d\n", wmbus_l_field[0]);
+                    fsk_raw_len = byte_size(packet_size(wmbus_l_field[0]));
+                    printf("wM-Bus frame length: %d\n", fsk_raw_len);
+                }
+                else {
+                    puts("Error decoding L-Field!");
+                    _print_bytes(l_field_raw, sizeof(l_field_raw));
+                    raw_data_cnt = 0;
+                    bit_cnt = 0;
+                    data = 0;
+                    memset(l_field_raw, 0, sizeof(l_field_raw));
+                    l_field_raw_cnt = 0;
+                    memset(wmbus_l_field, 0, sizeof(wmbus_l_field));
+                }
+            }
+        }
+        else {
+            if (fsk_raw_len) {
+                if (bit_cnt < 8) {
+                    fsk_raw_bytes[raw_data_cnt] = (fsk_raw_bytes[raw_data_cnt] << 1) | bit;
+                    bit_cnt++;
+                }
+                else {
+                    raw_data_cnt++;
+                    fsk_raw_len--;
+                    bit_cnt = 0;
+                }
+            }
+            else {
+                printf("received raw data: %d bytes\n", raw_data_cnt);
+                _print_bytes(fsk_raw_bytes, raw_data_cnt);
+                raw_data_cnt = 0;
+                bit_cnt = 0;
+                data = 0;
+                memset(l_field_raw, 0, sizeof(l_field_raw));
+                l_field_raw_cnt = 0;
+                memset(wmbus_l_field, 0, sizeof(wmbus_l_field));
+            }
+        }
+    }
 
-    if ((ps_detector & ACCESS_CODE_BITMASK) == ACCESS_CODE) {
+    if (((ps_detector & ACCESS_CODE_BITMASK) == ACCESS_CODE) && data == 0) {
         ps_detector = 0;
+        data = 1;
         printf("[main] preamble + sync word detected!\n");
     }
 }
 
 int fsk_init_cmd(int argc, char **argv)
 {
+    //netdev_t *netdev = (netdev_t*) &sx127x;
     int res;
 
-    //netdev_t *netdev = (netdev_t*) &sx127x;
     if (argc < 1) {
         puts("Usage: init_fsk <mode (packet, continuous)>");
         return -1;
@@ -110,14 +338,27 @@ int fsk_init_cmd(int argc, char **argv)
         res = gpio_init_int(sx127x.params.dio1_pin, GPIO_IN, GPIO_RISING, _dio1_data_recv, &sx127x);
         if (res < 0) {
             printf("[main] error: failed to initialize DIO1 pin\n");
-            return res;
+            return 0;
         }
 
         res = gpio_init(sx127x.params.dio2_pin, GPIO_IN);
         if (res < 0) {
             printf("[main] error: failed to initialize DIO2 pin\n");
-            return res;
+            return 0;
         }
+
+//        _sx127x_cont_pid = thread_create(sx1272_continuous_stack,
+//                                         sizeof(sx1272_continuous_stack),
+//                                         THREAD_PRIORITY_MAIN - 1,
+//                                         THREAD_CREATE_STACKTEST,
+//                                         sx127x_continuous_thread, NULL,
+//                                         "sx1272_cont_thread");
+
+//        if (_sx127x_cont_pid <= KERNEL_PID_UNDEF) {
+//            puts("Creation of sx127x continuous thread failed");
+//            return -1;
+//        }
+
     }
 
 
@@ -205,7 +446,7 @@ int random_cmd(int argc, char **argv)
 
     /* reinit the transceiver to default values */
     if (sx127x.settings.modem == SX127X_MODEM_FSK) {
-        sx127x_init_fsk_settings((sx127x_t*) netdev, SX127X_FSK_CONTINUOUS_MODE);
+        sx127x_init_fsk_settings((sx127x_t*) netdev, SX127X_FSK_PACKET_MODE);
     }
     else {
         sx127x_init_lora_settings((sx127x_t*) netdev);
@@ -308,6 +549,24 @@ int register_cmd(int argc, char **argv)
     return 0;
 }
 
+int register_check_cmd(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    puts("Listing registers...");
+
+    for (uint8_t reg = 1; reg < 127; reg++) {
+        if (sx127x_reg_read(&sx127x, reg) != fsk_reg_settings[reg - 1]){
+            printf("Mismatch register: 0x%02X -> 0x%02X ; 0x%02X\n", reg,
+                    sx127x_reg_read(&sx127x, reg), fsk_reg_settings[reg - 1]);
+        }
+    }
+    puts("");
+
+    return 0;
+}
+
 int send_cmd(int argc, char **argv)
 {
     if (argc <= 1) {
@@ -353,6 +612,7 @@ int listen_cmd(int argc, char **argv)
         /* Switch to RX state */
         uint8_t state = NETOPT_STATE_RX;
         netdev->driver->set(netdev, NETOPT_STATE, &state, sizeof(state));
+//        sx127x_set_state(&sx127x, SX127X_RF_RX_RUNNING);
     }
 
     printf("Listen mode set\n");
@@ -400,6 +660,7 @@ static const shell_command_t shell_commands[] = {
     { "send",     "Send raw payload string",                 send_cmd },
     { "listen",   "Start raw payload listener",              listen_cmd },
     { "init_fsk", "Initialise the radio in FSK mode",        fsk_init_cmd },
+    { "register_check", "Check register settings",           register_check_cmd},
     { NULL, NULL, NULL }
 };
 
@@ -412,7 +673,7 @@ static void _event_cb(netdev_t *dev, netdev_event_t event)
         msg.content.ptr = dev;
 
         if (msg_send(&msg, _recv_pid) <= 0) {
-            //puts("gnrc_netdev: possibly lost interrupt.");
+            puts("gnrc_netdev: possibly lost interrupt.");
         }
     }
     else {
@@ -420,8 +681,8 @@ static void _event_cb(netdev_t *dev, netdev_event_t event)
         netdev_sx127x_packet_info_t packet_info;
         switch (event) {
             case NETDEV_EVENT_RX_COMPLETE:
-                len = dev->driver->recv(dev, NULL, 0, 0);
                 if (sx127x.settings.modem == SX127X_MODEM_LORA) {
+                    len = dev->driver->recv(dev, NULL, 0, 0);
                     dev->driver->recv(dev, message, len, &packet_info);
                     printf("{Payload: \"%s\" (%d bytes), RSSI: %i, SNR: %i, TOA: %" PRIu32 "}\n",
                             message, (int)len,
@@ -429,12 +690,14 @@ static void _event_cb(netdev_t *dev, netdev_event_t event)
                             sx127x_get_time_on_air((const sx127x_t*)dev, len));
                 }
                 else {
-                    dev->driver->recv(dev, fsk_raw_bytes, len, &packet_info);
+                    DEBUG("Receiving data...\n");
+                    len = _recv(dev, NULL, 0, &packet_info);
+                    /*_recv(dev, fsk_raw_bytes, len, &packet_info);
                     printf("{Payload: ");
 
                     for (unsigned i = 0; i < len; i++) {
                         printf("%02X", fsk_raw_bytes[i]);
-                    }
+                    }*/
                     printf(" (%d bytes), RSSI: %i\n", (int)len, packet_info.rssi);
                 }
                 break;
